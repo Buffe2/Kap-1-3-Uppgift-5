1ea1298ac8a45bb580fddb61896487137b18ca2d
Admin <Admin@TEB123015>
2022-09-09 09:27:07 +0200

    Add project files.

---
+++ Kap 1-3 Uppgift 5/Kap_1_3_Uppgift_5.py
@@ -0,0 +1,8 @@
+L = input("Hur manga liter tankade du?")
+B = input("Hur mycket kostade bensinen per liter?")
+B1 = float(B)
+L1 = int(L)
+P=B1*L1 
+print(" --------------------------\n","|         Kvitto         |\n", "|  Tankad Volym", L1, "Liter |")
+print(" | Pris per liter", B1, "kr |\n", "| Betalt kroner", P, "   |\n","|   Valkommen Ater :)    |")
+print(" --------------------------")
